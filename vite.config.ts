cccc86c280c51e0e43728888eb31f7001715235c
Andy Lee <a.lee.intl@gmail.com>
2023-02-22 22:57:29 +0100

    transformMixedEsModules

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+/* import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs" */
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [/* viteCommonjs(), */ react()],
   server: {
     port: 3000,
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/linked-dep/, /node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['linked-dep'],
+    /* esbuildOptions: {
+      plugins: [esbuildCommonjs(["ws"])],
+    }, */
   },
 })
